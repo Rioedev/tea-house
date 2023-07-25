34cc7394a919df4df25a66f96c43a16ec37913f6
quyenncph25762 <viponline1610203@gmail.com>
2023-07-25 16:48:15 +0700

    admin-layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import "swiper/css";
 import LayoutClient from "./components/layouts/layout-client/LayoutClient";
 import HomePage from "./pages/client/HomePage";
 import ProductDetail from "./pages/client/productDetail";
+import LayoutAdmin from "./components/layouts/layout-admin/LayoutAdmin";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route index element={<HomePage />}></Route>
           <Route path="/productDetail" element={<ProductDetail />}></Route>
         </Route>
+        <Route path="/admin" element={<LayoutAdmin />}></Route>
       </Routes >
     </>
   );
---
+++ frontend/src/components/layouts/layout-admin/LayoutAdmin.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const LayoutAdmin = () => {
+    return (
+        <div>
+            admin
+        </div>
+    )
+}
+
+export default LayoutAdmin
