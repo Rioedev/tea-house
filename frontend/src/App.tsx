9d9b7f25a0d47af2d2e518e77286f11a9d5d7c2c
Rioedev <lequy11082003@gmail.com>
2023-07-27 23:44:34 +0700

    UI admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,12 @@ import CartPage from "./pages/client/CartPage";
 import ProductList from "./pages/client/ProductList";
 import Menus from "./pages/client/Menus";
 import LayoutAdmin from "./components/layouts/layout-admin/LayoutAdmin";
+import ListProduct from "./pages/admin/product/ListProduct";
+import AddProduct from "./pages/admin/product/AddProduct";
+import UpdateProduct from "./pages/admin/product/UpdateProduct";
+import ListCategory from "./pages/admin/category/ListCategory";
+import AddCategory from "./pages/admin/category/AddCategory";
+import UpdateCategory from "./pages/admin/category/UpdateCategory";
 
 function App() {
   return (
@@ -23,8 +29,15 @@ function App() {
           <Route path="/product-all" element={<ProductList />}></Route>
           <Route path="/menus" element={<Menus />}></Route>
         </Route>
-        <Route path="/admin" element={<LayoutAdmin />}></Route>
-      </Routes >
+        <Route path="/admin" element={<LayoutAdmin />}>
+          <Route path="product" element={<ListProduct />}></Route>
+          <Route path="add-product" element={<AddProduct />}></Route>
+          <Route path="update-product/:id" element={<UpdateProduct />}></Route>
+          <Route path="category" element={<ListCategory />}></Route>
+          <Route path="add-category" element={<AddCategory />}></Route>
+          <Route path="update-category" element={<UpdateCategory />}></Route>
+        </Route>
+      </Routes>
     </>
   );
 }
---
+++ frontend/src/pages/admin/product/UpdateProduct.tsx
@@ -0,0 +1,131 @@
+import { Link } from "react-router-dom";
+
+const UpdateProduct = () => {
+  return (
+    <div className="">
+      <Link
+        to={"/admin/product"}
+        className="px-5 py-4 font-medium text-white bg-green-500 rounded-lg shadow-lg shadow-green-500/50"
+      >
+        List product
+      </Link>
+      <h1 className="mb-10 text-3xl font-medium text-center text-white">
+        Thông tin sản phẩm
+      </h1>
+      <form>
+        <div className="grid gap-6 mb-6 md:grid-cols-2">
+          <div>
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Tên sản phẩm
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Tai nghe airpod"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="image"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Ảnh sản phẩm
+            </label>
+            <input
+              type="text"
+              id="image"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="price"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Price
+            </label>
+            <input
+              type="number"
+              id="price"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="123.434.000"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="originalPrice"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Original Price
+            </label>
+            <input
+              type="number"
+              id="originalPrice"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="123.45.678"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="brand"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Brand
+            </label>
+            <input
+              type="text"
+              id="brand"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Apple"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="origin"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Xuất xứ
+            </label>
+            <select
+              id="origin"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            >
+              <option>Choose a country</option>
+              <option value="Việt Nam">Việt Nam</option>
+              <option value="Trung Quốc">Trung Quốc</option>
+              <option value="United States">United States</option>
+              <option value="Canada">Canada</option>
+              <option value="France">France</option>
+              <option value="Germany">Germany</option>
+            </select>
+          </div>
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="message"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Description
+          </label>
+          <textarea
+            id="message"
+            className="block p-2.5 h-52 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Write your thoughts here..."
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          className="w-full px-5 py-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Update product
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default UpdateProduct;
